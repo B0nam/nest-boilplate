469466b8376df016000723f8480ba0c86a6bcf2d
kauanrossii <kauan.rossi@hotmail.com>
2024-10-07 19:51:56 -0300

    Instalação dos modulos config e mongoose, e utilização de .env

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { env } from 'process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      `mongodb://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
